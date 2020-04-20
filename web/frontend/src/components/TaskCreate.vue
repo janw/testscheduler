aaae9c33c4410e6baaff110ed07b80b50ea5a1a2
Jan Willhaus <mail@janwillhaus.de>
2020-04-20 10:35:21 +0200

    Add proper detailed validation feedback to TaskCreate

--- web/frontend/src/components/TaskCreate.vue
+++ web/frontend/src/components/TaskCreate.vue
@@ -9,6 +9,11 @@
           v-model="data.username"
           :state="valid_fields.username"
         ></b-input>
+        <b-form-invalid-feedback
+          v-bind:key="error"
+          v-for="error in errors.username"
+          :state="valid_fields.username"
+        >{{error}}</b-form-invalid-feedback>
       </b-col>
       <b-col cols="6" md="2">
         <label class="sr-only" for="inline-form-input-envid">Environment ID</label>
@@ -19,6 +24,11 @@
           v-model="data.env_id"
           :state="valid_fields.env_id"
         ></b-input>
+        <b-form-invalid-feedback
+          v-bind:key="error"
+          v-for="error in errors.env_id"
+          :state="valid_fields.env_id"
+        >{{error}}</b-form-invalid-feedback>
       </b-col>
       <b-col class="mt-2 mt-md-0">
         <label class="sr-only" for="inline-form-input-testpath">Test Path</label>
@@ -29,6 +39,11 @@
           v-model="data.path"
           :state="valid_fields.path"
         ></b-form-input>
+        <b-form-invalid-feedback
+          v-bind:key="error"
+          v-for="error in errors.path"
+          :state="valid_fields.path"
+        >{{error}}</b-form-invalid-feedback>
 
         <datalist id="testpath-list">
           <option v-bind:key="path" v-for="path in paths">{{ path }}</option>
@@ -38,13 +53,6 @@
         <b-button :disabled="submit_disabled" type="submit" variant="primary">Submit</b-button>
       </b-col>
       <div class="w-100"></div>
-      <b-col>
-        <b-form-invalid-feedback
-          v-bind:key="error"
-          v-for="error in errors"
-          :state="noErrors"
-        >{{error}}</b-form-invalid-feedback>
-      </b-col>
     </b-form-row>
   </b-form>
 </template>
@@ -53,20 +61,19 @@
 export default {
   data() {
     return {
-      errors: [],
-      validation_message: "",
+      errors: {},
       valid_fields: {
         username: null,
         env_id: null,
         path: null
       },
-      submit_disabled: false,
-      paths: [],
       data: {
         username: "",
         env_id: "",
         path: ""
-      }
+      },
+      paths: [],
+      submit_disabled: false
     };
   },
   created() {
@@ -77,11 +84,6 @@ export default {
         this.paths = response.data;
       })
       .catch(error => {});
-  },
-  computed: {
-    noErrors() {
-      return this.errors.length == 0;
-    }
   },
   methods: {
     handleSubmit() {
@@ -98,9 +100,9 @@ export default {
         })
         .catch(error => {
           // Translate API validation results into frontend error displayal
-          for (var field in error.data.message) {
+          for (var field in error.data.errors) {
             this.valid_fields[field] = false;
-            this.errors.push(error.data.message[field]);
+            this.errors = error.data.errors;
           }
         })
         .then(() => {
